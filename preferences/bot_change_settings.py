04b72008503b75493291bb6bd97b53c417b236c3
a-chubarev <a-chubarev@mail.ru>
2022-11-06 01:43:45 +0600

    Изменил логику запросов

--- preferences/bot_change_settings.py
+++ preferences/bot_change_settings.py
@@ -6,16 +6,14 @@ preferences_path = '../file_storage/bot preferences.ini'
 
 # возвращает True если есть файл с настройками бота
 def checking_preferences_file(pref_path):
-    file_availability = os.path.isfile(preferences_path)
+    file_availability = os.path.isfile(pref_path)
     return file_availability
 
 
 # Вывод сообщения если файл настроек не создан
 def return_first_launch_message(pref_path):
-    if not checking_preferences_file(pref_path):
-        first_launch_text = "Greetings, at the next step, enter the necessary " \
-                            "information about the bot for further work"
-        return first_launch_text
+    first_launch_text = "At the next step, enter the necessary information about the bot for further work"
+    return first_launch_text
 
 
 # Создание ini файла с шаблоном настроек бота
@@ -32,6 +30,7 @@ def write_bot_name(pref_path):
     config = configparser.ConfigParser()
     if not checking_preferences_file(pref_path):
         create_preferences_file(pref_path)
+    config.read(pref_path)
     bot_name = input('Enter the name of the bot: ')
     config.set('Preferences', 'Name', f'{bot_name}')
     with open(pref_path, 'w') as config_file: config.write(config_file)
@@ -42,6 +41,7 @@ def write_bot_token(pref_path):
     config = configparser.ConfigParser()
     if not checking_preferences_file(pref_path):
         create_preferences_file(pref_path)
+    config.read(pref_path)
     bot_token = input('Enter the bot token: ')
     config.set('Preferences', 'Token', f'{bot_token}')
     with open(pref_path, 'w') as config_file: config.write(config_file)
@@ -51,6 +51,7 @@ def write_bot_token(pref_path):
 def return_bot_name(pref_path):
     config = configparser.ConfigParser()
     if checking_preferences_file(pref_path):
+        config.read(pref_path)
         bot_name = config.get('Preferences', 'Name')
         if bot_name != '':
             return bot_name
@@ -63,6 +64,7 @@ def return_bot_name(pref_path):
 def return_bot_token(pref_path):
     config = configparser.ConfigParser()
     if checking_preferences_file(pref_path):
+        config.read(pref_path)
         bot_token = config.get('Preferences', 'Token')
         if bot_token != '':
             return bot_token
@@ -72,13 +74,48 @@ def return_bot_token(pref_path):
         return 'Something went wrong: preferences file not exist!'
 
 
+# Изменение конфига бота
+def change_bot_config(pref_path):
+    print(return_first_launch_message(pref_path))
+    write_bot_name(pref_path)
+    write_bot_token(pref_path)
+
+
 # Возвращает приветственное сообщение с именем бота если существует файл конфигурации
-def return_welcome_message():
-    if checking_preferences_file(preferences_path):
-        bot_name = return_bot_name(preferences_path)
-        welcome_text = f'Welcome to {bot_name}!'
+def return_welcome_message(pref_file):
+    if checking_preferences_file(pref_file):
+        bot_name = return_bot_name(pref_file)
+        bot_token = return_bot_token(pref_file)
+        welcome_text = f'Welcome to {bot_name}! \n' \
+                       f'Bot token: {bot_token}'
     return welcome_text
 
 
-print(checking_preferences_file())
-create_preferences_file(preferences_path)
+# проверяет согласие юзера с выбранным конфигом бота
+def checking_agreement_with_configuration():
+    print('Are you agreement with the bot configuration?')
+    user_input = input('y - yes/ n - no: ')
+    if user_input == 'у' or user_input == 'Y' or user_input == 'y' or user_input == 'e' or user_input == 'E' or \
+            user_input == 'н' or user_input == 'Н' or user_input == 'У':
+        return False
+    else:
+        return True
+
+
+# Проверка настроек и изменение при запуске
+def initial_check():
+    # TODO: по идее надо переписывать логику
+    if not checking_preferences_file(preferences_path):
+        create_preferences_file(preferences_path)
+        change_bot_config(preferences_path)
+    print (return_welcome_message(preferences_path))
+    x = checking_agreement_with_configuration()
+    if x:
+        change_bot_config(preferences_path)
+    else:
+        print('Start')
+
+
+
+
+initial_check()
